579f7c0714bdbe3b1b6414a611332f99921f57e0
charlie166 <charlie166@163.com>
2017-11-16 17:13:44 +0800

    JSOUP测试

---
+++ project/tools/src/test/java/cn/charlie166/web/store/tools/test/JsoupTest.java
@@ -0,0 +1,38 @@
+package cn.charlie166.web.store.tools.test;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+/**
+* @ClassName: JsoupTest 
+* @Description: JSOUP工具测试类
+* @company 
+* @author liyang
+* @Email charlie166@163.com
+* @date 2017年11月16日 
+*
+ */
+public class JsoupTest {
+
+	@Test
+	public void testHtml(){
+		try {
+			Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+			System.out.println("title:" + doc.title());
+			Elements newsHeadlines = doc.select("#mp-itn b a");
+			for (Element headline : newsHeadlines) {
+				String linkTitle = headline.attr("title");
+				String absUrl = headline.absUrl("href");
+				String string = String.format("%s\t%s", linkTitle, absUrl);
+				System.out.println(string);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
